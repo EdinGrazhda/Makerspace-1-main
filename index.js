dec411f6094c7bffe1a6c7ea49a1f1d3d8a508a7
DESKTOP-732D0OO\Edin <edingrazhda17@gmail.com>
2023-09-19 18:55:14 +0200

    javascript basics

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+let a = 'string';
+let b = 1;
+let c;
+let d = true;
+let e = 1.1;
+let r = [];
+let t = {};
+let y = null;
+
+a=b // a eshte barabart me b
+a==b// a a eshte barabart me b
+a===b// tipi dhe vlera e a-s a eshte  barabart me b
+
+console.log(typeof a);
+
