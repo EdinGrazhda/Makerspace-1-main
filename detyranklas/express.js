84c9f0a550b6e08d931ea071ecfe325a8a762c72
DESKTOP-732D0OO\Edin <edingrazhda17@gmail.com>
2023-10-10 18:56:04 +0200

    express js

---
+++ detyranklas/express.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.post('/',(req,res)=>{
+    res.send("This is a post method!");
+})
+app.put('/',(req,res)=>{
+    res.send("This is a put method !!");
+})
+app.delete('/',(req,res)=>{
+    res.send("This is a delete method !!!!");
+})
+
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
