52ede89e63f914c65eec0b457ff13a748e95b4c7
DESKTOP-732D0OO\Edin <edingrazhda17@gmail.com>
2023-10-24 18:16:20 +0200

    update

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,37 @@
-const getUsers =(req,res)=>{
-    res.send('this is a GET method!');
-}
-module.exports={getUsers};
+const getUsers = (req, res) => {
+    res.send('This is a GET method!!!!!!!');
+};
+
+const createUser = (req, res) => {
+    res.send('This is a post method!!!!!!!');
+};
+
+const updateUser = (req, res) => {
+    res.send('This is a Update method!!!!!!!');
+};
+
+const deleteUser = (req, res) => {
+    res.send('This is a Delete method!!!!!!!');
+};
+
+const getUser = (req, res) => {
+    let users = [
+        {
+            id: 1,
+            name: 'Hadi',
+        },
+        { id: 2, name: 'Mumin' },
+        { id: 3, name: 'Ali' },
+        { id: 4, name: 'Enis' },
+    ];
+
+    let selectedUser = [];
+    for (let user of users) {
+        if (req.params.id == user.id) {
+            selectedUser = user;
+        }
+    }
+    res.send(selectedUser);
+};
+
+module.exports = { getUsers, createUser, updateUser, deleteUser, getUser };
--- routers/users.js
+++ routers/users.js
@@ -1,7 +1,16 @@
-const router = require("express").Router();
-const{getUsers,postUsers,putUsers,deleteUsers}=require("../controllers/users.js")
-router.get('/',getUsers)
-router.post('/',postUsers)
-router.put('/',putUsers)
-router.delete('/',deleteUsers)
+const router = require('express').Router();
+const {
+    getUsers,
+    createUser,
+    deleteUser,
+    updateUser,
+    getUser,
+} = require('../controllers/users');
 
+router.get('/', getUsers);
+router.get('/:id', getUser);
+router.post('/', createUser);
+router.put('/', updateUser);
+router.delete('/', deleteUser);
+
+module.exports = router;
