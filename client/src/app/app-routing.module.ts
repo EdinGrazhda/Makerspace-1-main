42c3039e0c37a7ad20078b8763b13afe05db70cb
DESKTOP-732D0OO\Edin <edingrazhda17@gmail.com>
2023-11-09 17:10:09 +0100

    angular

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [{
+  path:'',
+  pathMatch:'full',
+  redirectTo:'login',
+},
+{
+  path:'login',
+  component: LoginComponent,
+},
+{
+  path:'**',
+  component:NotFoundComponent,
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  arr = [
+    { id: 1, name: 'Apple', describe: 'kingi', price: 1000 },
+    { id: 2, name: 'Samsung', describe: 'tamtung', price: 700 },
+    { id: 3, name: 'Xiaomi', describe: 'kinez', price: 450 },
+  ];
+}
